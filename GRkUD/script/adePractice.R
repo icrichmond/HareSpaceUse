5de79fc1241f85987f4e81884a6ba640f4003f6d
icrichmond <icrichmond@mun.ca>
2020-02-25 12:04:09 -0330

    Update adePractice.R

--- GRkUD/script/adePractice.R
+++ GRkUD/script/adePractice.R
@@ -74,3 +74,48 @@ ii <- estUDm2spixdf(kus)
 class(ii)
 # Can make the grid equal to a different raster (e.g. environmental variables)
 kudm <- kernelUD(puechabonsp$relocs[,1], grid = puechabonsp$map)
+
+
+### Estimating home ranges from UD ###
+# Estimate the 90% home range using LSCV algorithm in vector form
+homerange <- getverticeshr(kud)
+class(homerange)
+plot(homerange, col=1:4)
+# Can also estimate in raster mode from the UD 
+vud <- getvolumeUD(kud)
+vud
+# Look at differences between the output of kernelUD and getvolumeUD 
+par(mfrow=c(2,1))
+par(mar=c(0,0,2,0))
+# Output of kernelUD for first animal
+image(kud[[1]])
+title("Output of kernelUD")
+# Convert into suitable data structure for the use of contour 
+xyz <- as.image.SpatialGridDataFrame(kud[[1]])
+contour(xyz, add=TRUE)
+# Now for getvolumeUD
+par(mar=c(0,0,2,0))
+image(vud[[1]])
+title("Output of getvolumeUD")
+xyzv <- as.image.SpatialGridDataFrame(vud[[1]])
+contour(xyzv, add=TRUE)
+# store the volume under the UD of the first animal 
+fud <- vud[[1]]
+# Store the value of volume under UD in a vector hr95
+hr95 <- as.data.frame(fud)[,1]
+# If hr95 is <= 95 then the pixel belongs to the home range (takes value 1,0 otherwise)
+hr95 <- as.numeric(hr95 <= 95)
+# Convert into data frame 
+hr95 <- data.frame(hr95)
+# Convert to SpatialPixelsDataFrame
+coordinates(hr95) <- coordinates(vud[[1]])
+gridded(hr95) <- TRUE
+image(hr95)
+# Can also calculate the area of the home range - can do this for several probability levels
+ii <- kernel.area(kud, percent = seq(50,95, by=5))
+ii
+plot(ii)
+# Home range sizes returned this way are different than SpatialPolygonsDataFrame that is returned
+# using getverticeshr. Different is smaller when grid resolution is finer 
+# Can also take into account physical boundaries of the study area - skipping example as BL 
+# does not have any physical barriers
