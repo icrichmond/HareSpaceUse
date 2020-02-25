ff4caf9fa897418eab17fcd538109203bf53f59e
Isabella Richmond <Isabella Richmond@DESKTOP-SF8TV8G.local>
2020-02-25 09:50:36 -0330

    Adding GR's R project and first script

---
+++ GRkUD/script/adePractice.R
@@ -0,0 +1,8 @@
+# This code is running through the examples in the documentation for the adehabitatHR package
+# Project is part of a git repository for GR's honours (kernels)
+
+# Load package
+library(adehabitatHR)
+
+# Loading required data
+xy <- matrix(runif(60), ncol=2)
