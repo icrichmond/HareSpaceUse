254e7c1557dbec7043728ea3563783e28305a3d9
icrichmond <icrichmond@mun.ca>
2020-02-25 16:00:02 -0330

    Update KernelEstimation.R

--- Bella_kUD/BellakUD/script/KernelEstimation.R
+++ Bella_kUD/BellakUD/script/KernelEstimation.R
@@ -2,3 +2,19 @@
 # over three years (2016-2019). Relocations only taken in summer season.
 # Author: Isabella Richmond
 # Last Edited: February 25, 2020
+
+# load required packages 
+easypackages::packages("sp", "maptools", "tmap", "tmaptools", 
+              "adehabitatHR", "adehabitatHS", "adehabitatLT", "ellipse", 
+              "nleqslv", "adehabitatMA", "adehabitatHR","dplyr", "gdtools", "ggmap", "ggplot2", 
+              "ggrepel", "ggsci", "ggthemes", "maps", "raster", "spatial", 
+              "tidyr", "readr","rgdal", "rgeos", "reshape2", "dismo")
+
+### NOTE: one package (sigloc) is no longer maintained - need to manaully load package ###
+
+# import telemetry dataset
+VHF <- read_csv("input/TelemetryPoints_VHF_2019.csv")
+View(VHF)
+
+# following section developed from Matteo Rizzuto's code
+# process the dataset so it is ready for home range estimation.
