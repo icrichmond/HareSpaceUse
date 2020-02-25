7acd5506432f9aa2846818ebe270bea913c8bb92
icrichmond <icrichmond@mun.ca>
2020-02-25 15:46:27 -0330

    Update .gitignore, BellakUD.Rproj, and 3 more files...

---
+++ Bella_kUD/BellakUD/script/KernelEstimation.R
@@ -0,0 +1,4 @@
+# This script is for estimating the space use of snowshoe hare collared in Bloomfield, NL 
+# over three years (2016-2019). Relocations only taken in summer season.
+# Author: Isabella Richmond
+# Last Edited: February 25, 2020
