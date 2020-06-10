b06105435c5e57553dd758a522d33d92fd7cf024
icrichmond <icrichmond@mun.ca>
2020-06-10 11:15:57 -0230

    Created script for ordination of predation risk variables
    Former-commit-id: 003dc96597ce01e57ca6a26bcbf707945372e76c

---
+++ Bella_kUD/BellakUD/script/HabitatComplexity.R
@@ -0,0 +1,13 @@
+# This script is for creating an index of habitat complexity/predation risk for snowshoe hares
+# in Newfoundland, Canada. All habitat data was collected in July 2019 (after green-up)
+
+# Author: Isabella Richmond
+# Last Edited: June 00, 2020
+
+# load required packages 
+library(easypackages)
+easypackages::packages("tidyverse", "vegan")
+
+# load required data
+
+
