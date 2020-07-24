cf83fc20d73cbf76b6388e260ead68a0595078a2
icrichmond <icrichmond@mun.ca>
2020-07-24 09:31:26 -0230

    Updated home range figure with grid points
    Former-commit-id: 145bda990b3e5f0a715beb0ed201fd0c389a97aa

--- Bella_kUD/BellakUD/script/X - Graphics.R
+++ Bella_kUD/BellakUD/script/X - Graphics.R
@@ -73,8 +73,8 @@ dev.off()
 
 # read in raster list
 kernel95 <- readRDS("large/akderasters.rds")
-# removing 149.394 because it is too large to be biologically relevant
-kernel95[[7]] <- NULL
+# removing 149.555 because it was sampled across multiple years
+kernel95[[12]] <- NULL
 # transform raster values so they are between 0,1
 kernel95norm <- lapply(kernel95, spatialEco::raster.transformation)
 # use clamp to transform any values under 0.15 to Nas
@@ -92,17 +92,31 @@ over <- raster::overlay(kernel95norms, fun=sum)
 # read in complexity sampling points
 bl_cs_pts <- read_sf("input/Mapping", layer = "cs_points")
 bl_cs_pts <- sf::st_transform(bl_cs_pts, "+init=epsg:32622")
+# read in trap sampling points
+bl_grid_pts <- read_sf("input/Mapping", layer = "bl_grid_points")
+bl_grid_pts <- sf::st_transform(bl_grid_pts, "+init=epsg:32622")
 # set extent to include all home ranges but not be too large
 e <- raster::extent(278000,280000,5359000,5360500)
 # plot merged data - individuals 
-tm_shape(kernel95normm, bbox=e)+
+t <- tm_shape(kernel95normm, bbox=e)+
   tm_raster(title = "kUD", style = "cont", 
             palette = "-RdYlBu", alpha=0.9)+
   tm_scale_bar()+
-  tm_layout(legend.bg.color = "white")+
   tm_grid()+
+  tm_xlab("Easting")+
+  tm_ylab("Northing")+
 tm_shape(bl_cs_pts)+
-  tm_dots(size = 0.15)
+  tm_dots(size = 0.15)+
+tm_shape(bl_grid_pts)+
+  tm_dots(size = 0.15, shape = 4)+
+tm_legend()+
+tm_layout(legend.bg.color = "white")
+t
+tmap_save(t, "graphics/kUD_raster_grids.png")
+
+plot(bl_cs_pts$geometry)
+plot(bl_grid_pts$geometry, add=T)
+
 # plot overlay data - populations
 tm_shape(over, bbox=e)+
   tm_raster(title = "kUD", style = "cont", 
