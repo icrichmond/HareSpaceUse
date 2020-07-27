33ae679d9547e34920a383876c9e4006d42ae165
icrichmond <icrichmond@mun.ca>
2020-07-27 11:06:23 -0230

    Plotting code included
    Former-commit-id: 6bf5144f92019df46e8d24a32aebc6bf1437e6b0

--- Bella_kUD/BellakUD/script/5 - HomeRangeExtraction.R
+++ Bella_kUD/BellakUD/script/5 - HomeRangeExtraction.R
@@ -1,5 +1,5 @@
 # Author: Isabella Richmond
-# Last Edited: July 22, 2020
+# Last Edited: July 27, 2020
 
 # This script is for extracting the food quality and kUD values at each habitat complexity
 # sampling point.
@@ -98,3 +98,68 @@ cskudpop <- as.data.frame(cskudpop)
 stoichkudpop <- cbind(cskudpop, csstoich)
 stoichkudpop <- add_column(stoichkudpop, Plot = predrisk$Plot)
 write.csv(stoichkudpop, "output/cs_stoich_kud_pop.csv")
+
+# --------------------------------------- #
+#              Visualize Data             #
+# --------------------------------------- #
+#### Home Ranges --------
+# use clamp to transform any values under 0.15 to Nas
+kernel95normz <- lapply(kernel95norm, function(i) raster::clamp(i, lower=0.15, useValues=FALSE))
+# save clamped raster
+saveRDS(kernel95normz, "large/rasternormclamp.rds")
+clam <- readRDS("large/rasternormclamp.rds")
+# stack clamped rasters and merge them - can see individuals when plotted 
+clams <- raster::stack(clam)
+kernel95normm <- raster::merge(clams)
+# stack original raster list and overlay, summing values where there 
+# is more than one raster - shows collective when plotted
+kernel95norms <- raster::stack(kernel95norm)
+over <- raster::overlay(kernel95norms, fun=sum)
+# read in complexity sampling points
+bl_cs_pts <- read_sf("input/Mapping", layer = "cs_points")
+bl_cs_pts <- sf::st_transform(bl_cs_pts, "+init=epsg:32622")
+# read in trap sampling points
+bl_grid_pts <- read_sf("input/Mapping", layer = "bl_grid_points")
+bl_grid_pts <- sf::st_transform(bl_grid_pts, "+init=epsg:32622")
+# set extent to include all home ranges but not be too large
+e <- raster::extent(278000,280000,5359000,5360500)
+# plot merged data - individuals 
+t <- tm_shape(kernel95normm, bbox=e)+
+  tm_raster(title = "kUD", style = "cont", 
+            palette = "-RdYlBu", alpha=0.9)+
+  tm_scale_bar()+
+  tm_grid()+
+  tm_xlab("Easting")+
+  tm_ylab("Northing")+
+  tm_shape(bl_cs_pts)+
+  tm_dots(size = 0.15)+
+  tm_shape(bl_grid_pts)+
+  tm_dots(size = 0.15, shape = 4)+
+  tm_legend()+
+  tm_layout(legend.bg.color = "white")
+t
+tmap_save(t, "graphics/kUD_raster_grids.png")
+
+plot(bl_cs_pts$geometry)
+plot(bl_grid_pts$geometry, add=T)
+
+# plot overlay data - populations
+tm_shape(over, bbox=e)+
+  tm_raster(title = "kUD", style = "cont", 
+            palette = "-RdYlBu", alpha=0.9)+
+  tm_scale_bar()+
+  tm_layout(legend.bg.color = "white")+
+  tm_grid()+
+  tm_shape(bl_cs_pts)+
+  tm_dots(size = 0.15)
+
+
+ade <- read.csv("output/homerangeareas.csv")
+ade <- filter(ade, Kernel == 95)
+hr_df <- hr_df[-c(12,22),]
+all <- bind_cols(ade, hr_df)
+
+ggplot(all, aes(x=value, y=homerange))+
+  geom_point()+
+  labs(x = "AdeHabitat Area (ha)", y = "ctmm Area (ha)", title = "95% Home Ranges")
+cor(all$value, all$homerange)
