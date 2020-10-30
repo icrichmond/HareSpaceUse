bd635156e8dd227e32d023b74115e69477471f70
icrichmond <icrichmond@mun.ca>
2020-10-30 13:50:47 -0230

    Rerunning analysis

--- script/8 - Maps.R
+++ script/8 - Maps.R
@@ -1,5 +1,5 @@
 # Author: Isabella Richmond
-# Last Edited: September 2, 2020
+# Last Edited: October 30, 2020
 
 # This script is for creating figures/maps for this dataset
 
@@ -17,7 +17,7 @@ clams <- raster::stack(clam)
 kernel95normm <- raster::merge(clams)
 # stack original raster list and overlay, summing values where there 
 # is more than one raster - shows collective when plotted
-kernel95norms <- raster::stack(kernel95norm)
+kernel95norms <- raster::stack(kernel95normm)
 over <- raster::overlay(kernel95norms, fun=sum)
 # read in complexity sampling points
 bl_cs_pts <- read_sf("input/Mapping", layer = "cs_points")
@@ -25,8 +25,17 @@ bl_cs_pts <- sf::st_transform(bl_cs_pts, "+init=epsg:32622")
 # read in trap sampling points
 bl_grid_pts <- read_sf("input/Mapping", layer = "bl_grid_points")
 bl_grid_pts <- sf::st_transform(bl_grid_pts, "+init=epsg:32622")
+# read in stoich data 
+vaancn <- raster("input/VAAN_CN.tif")
+vaancp <- raster("input/VAAN_CP.tif")
+# reproject rasters 
+vaancn <- projectRaster(vaancn, crs="+init=epsg:32622")
+vaancp <- projectRaster(vaancp, crs="+init=epsg:32622")
 # set extent to include all home ranges but not be too large
 e <- raster::extent(278000,280000,5359000,5360500)
+# crop rasters
+vaancnclip <- crop(vaancn, e)
+vaancpclip <- crop(vaancp, e)
 
 # NL boundary layer (code from https://github.com/wildlifeevoeco/study-area-figs)
 zz <- opq(getbb('Newfoundland')) %>%
